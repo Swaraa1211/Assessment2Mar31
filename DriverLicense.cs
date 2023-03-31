8eb62d570bf469a320b88c88534cb281e7c79dd6
Swaraa1211 <saiswaroopasaranath1211@gmail.com>
2023-03-31 16:46:35 +0530

    Add project files.

---
+++ DriverLicense.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment2Mar31
+{
+    internal class DriverLicense
+    {
+        public static void Main(string[] args)
+        {
+            DriverLicense obj = new DriverLicense();
+            Console.WriteLine("Hello!");
+            
+            /*string name = "Eric";
+            int agent = 2;
+            List<string> other_people = new List<string>();
+            other_people.Add("Adam");
+            other_people.Add("Caroline");
+            other_people.Add("Rebecca");
+            other_people.Add("Frank");*/
+
+            
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("\nAgent: ");
+            int agent = Convert.ToInt32(Console.ReadLine());
+
+            List<string> other_people = new List<string>();
+            for(int i=0; i<4; i++)
+            {
+                Console.Write("\nOthers: ");
+                var names = Console.ReadLine();
+                other_people.Add(names);
+            }
+
+             
+
+
+            obj.License(name,agent, other_people);
+            Console.WriteLine("Thank You!");
+        }
+
+        public void License(string name,int agent, List<string> other_people)
+        {
+            other_people.Add(name);
+            other_people.Sort();
+            int num = other_people.FindIndex(a => a.Contains(name));
+            Console.WriteLine($"In {num*20} mins, your license would be processed");
+        }
+    }
+}
---
+++ LicensePlate.cs
@@ -0,0 +1,66 @@
+﻿namespace Assessment2Mar31
+{
+    internal class LicensePlate
+    {
+        static void Main(string[] args)
+        {
+            LicensePlate licensePlate = new LicensePlate();
+            Console.WriteLine("Welcome !");
+            /*string plate_name = "5F3Z-2e-9-w";
+            int length = 4;*/
+            
+            Console.Write("Number Plate: ");
+            string plate_name = Console.ReadLine();
+            
+            Console.Write("Length: ");
+            int length = Convert.ToInt32(Console.ReadLine());
+            
+            licensePlate.Plate(plate_name, length);
+        }
+
+        public void Plate(string plateName, int length)
+        {
+            
+            int count = -1,len = plateName.Length;
+            char[] arr_plateName = plateName.ToCharArray();
+
+            Array.Reverse(arr_plateName);
+
+            char[] numberPlate = new char[len];
+            char[] namePlate = new char[100] ;
+            
+            for(int i=0; i< arr_plateName.Length; i++)
+            {
+                if (arr_plateName[i] == '-')
+                {
+                    //arr_plateName = arr_plateName.Skip(i).ToArray();
+                    i++;
+                }
+                if (arr_plateName[i] >= 'a' && arr_plateName[i] <= 'z')
+                {
+                    numberPlate[i] += (char)(arr_plateName[i] - 'a' + 'A');
+                }
+                else
+                    numberPlate[i] += arr_plateName[i];
+            }
+            //Console.WriteLine(numberPlate);
+            for (int i = 0; i < numberPlate.Length; i++)
+            {
+                namePlate[i] = numberPlate[i];
+                if(count == length)
+                {
+                    i++;
+                    namePlate[i] = '-';
+                    count = -1;
+                }
+                count++;
+            }
+            Array.Reverse(namePlate);
+            Console.WriteLine(namePlate);
+        }
+    }
+}
+//plateName.ToUpper();
+//string revPlate = (string)plateName.Reverse();
+//Console.WriteLine(arr_plateName);
+//char[] rev_plateName = arr_plateName.Reverse();
